12d80088216a81b41bd218872c22cf3d9c070d26
JiSeong Kim <12180543@inha.edu>
2023-01-12 16:11:52 +0900

    dict03

--- day01.py
+++ day01.py
@@ -1,10 +1,7 @@
-# for countdown in 5,4,3,2,1, "hey!":
-#     print(countdown)
-#
-# print('프로그램 종료')
+subjects = {'의사소통영어':'A', '오래된 미래':'B+', '양자역학':'A0'}
 
-countdown_lst = [5,4,3,2,1, "hey!"]
-for countdown in countdown_lst:
-    print(countdown)
-print(countdown_lst[3])
-print('프로그램 종료')
+student = '김도훈'
+subject = '오래된 미래'
+
+print('%s학생의 %s과목 성적은 %s입니다.' % (student, subject, subjects[subject]))
+print('{}학생의 {}과목 성적은 {}입니다.'.format(student, subject, subjects[subject]))

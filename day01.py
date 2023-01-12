5b81e9860fcd435935112b47f2121508c0846292
JiSeong Kim <12180543@inha.edu>
2023-01-12 16:12:15 +0900

    gui04

--- day01.py
+++ day01.py
@@ -1,7 +1,6 @@
-subjects = {'의사소통영어':'A', '오래된 미래':'B+', '양자역학':'A0'}
+import tkinter as tk
 
-student = '김도훈'
-subject = '오래된 미래'
-
-print('%s학생의 %s과목 성적은 %s입니다.' % (student, subject, subjects[subject]))
-print('{}학생의 {}과목 성적은 {}입니다.'.format(student, subject, subjects[subject]))
+win = tk.Tk()
+win.geometry('400x300')
+win.title('파이썬 1일차 preview')
+win.mainloop()

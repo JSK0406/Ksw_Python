8122db354d1de90615f6bfb0910a333382754c7c
JiSeong Kim <12180543@inha.edu>
2023-01-16 14:08:44 +0900

    day03_while

--- day03.py
+++ day03.py
@@ -1,40 +1,23 @@
-univ = 'Inha University'
+# while True:
+#     dan = int(input('dan : '))
+#     cnt = 1
+#     if dan == 0:
+#         break
+#     if 2 <= dan <= 9:
+#         while cnt <= 9:
+#             print((f'{dan} * {cnt} = {dan * cnt}'))
+#             cnt += 1
+#     else:
+#         continue
+#
 
-# print(univ[5:])
-# print(univ[5:15])
-# print(univ[-10:])
-# print(univ[::2])
-# print(univ[5:-6])
-# print(univ[-10:-6])
-# print(len(univ))
-
-# print(univ.split('i'))
-# pokemons_lst = ['피카츄', '꼬부기', '이상해씨', '파이리']
-# pokemons_str = ', '.join(pokemons_lst)
-# print(pokemons_str)
-
-# inha = 'a duck goes into a sea'
-# print(inha.replace('a','a famous'))
-# print(inha.replace('a ','a famous '))
-
-# world = "  earth "
-# print(world.strip(), '입니다')
-# print(world.strip(' '), '입니다')
-# print(world.rstrip(), '입니다')
-# print(world.lstrip(), '입니다')
-# test = "DABCDDD"
-# print(test.strip('D'))
-# print(test.rstrip('D'))
-# print(test.lstrip('D'))
-# print(test.strip('DBA')) !!strip안의 조합으로 이루어진 문자열 제거!!
-
-# subjects = 'python, data structure, database'
-# print(subjects.find('data'))
-# print(subjects.index('data'))
-# print(subjects.find('inha')) #-1
-# print(subjects.index('inha')) #exception
-# print(subjects.rfind('data'))
-# print(subjects.rindex('data'))
-# print(subjects.count('data'))
-# print(subjects.title())
+# numbers = [1,3,5,4]
+# position = 0
+# while position < len(numbers):
+#     number = numbers[position]
+#     if number % 2 == 0:
+#         print('Found even number', number)
+#     position += 1
+# else:   !!breaker checker!!
+#     print('No even number found')
 

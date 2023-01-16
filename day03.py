cb8c54dd457d7cf59601ac2ade3fe7946cda5010
JiSeong Kim <12180543@inha.edu>
2023-01-16 11:38:48 +0900

    day03_text_indexing_functions

--- day03.py
+++ day03.py
@@ -1,13 +1,40 @@
-text1 = 'A\n'\     여러 줄로 작성(text사이에 \를 넣으면 엔터를 넣어도 괜찮다)
-    'B'
+univ = 'Inha University'
 
-text2 = '''A
-B'''
-print(text1)
-print(text2)
+# print(univ[5:])
+# print(univ[5:15])
+# print(univ[-10:])
+# print(univ[::2])
+# print(univ[5:-6])
+# print(univ[-10:-6])
+# print(len(univ))
 
-test1 = 'A\nB'
-test2 = 'A\tB'
+# print(univ.split('i'))
+# pokemons_lst = ['피카츄', '꼬부기', '이상해씨', '파이리']
+# pokemons_str = ', '.join(pokemons_lst)
+# print(pokemons_str)
 
-print(test1)
-print(test2)
+# inha = 'a duck goes into a sea'
+# print(inha.replace('a','a famous'))
+# print(inha.replace('a ','a famous '))
+
+# world = "  earth "
+# print(world.strip(), '입니다')
+# print(world.strip(' '), '입니다')
+# print(world.rstrip(), '입니다')
+# print(world.lstrip(), '입니다')
+# test = "DABCDDD"
+# print(test.strip('D'))
+# print(test.rstrip('D'))
+# print(test.lstrip('D'))
+# print(test.strip('DBA')) !!strip안의 조합으로 이루어진 문자열 제거!!
+
+# subjects = 'python, data structure, database'
+# print(subjects.find('data'))
+# print(subjects.index('data'))
+# print(subjects.find('inha')) #-1
+# print(subjects.index('inha')) #exception
+# print(subjects.rfind('data'))
+# print(subjects.rindex('data'))
+# print(subjects.count('data'))
+# print(subjects.title())
+

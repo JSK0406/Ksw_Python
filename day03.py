0af10806d0b9654c024c802d586b7d217954c3d7
JiSeong Kim <12180543@inha.edu>
2023-01-16 14:45:10 +0900

    day03_for

--- day03.py
+++ day03.py
@@ -1,23 +1,47 @@
 # while True:
 #     dan = int(input('dan : '))
-#     cnt = 1
 #     if dan == 0:
 #         break
 #     if 2 <= dan <= 9:
-#         while cnt <= 9:
-#             print((f'{dan} * {cnt} = {dan * cnt}'))
-#             cnt += 1
+#         for i in range(1, 10):
+#             print(f'{dan} * {i} = {dan * i}')
 #     else:
 #         continue
-#
 
-# numbers = [1,3,5,4]
-# position = 0
-# while position < len(numbers):
-#     number = numbers[position]
-#     if number % 2 == 0:
-#         print('Found even number', number)
-#     position += 1
-# else:   !!breaker checker!!
-#     print('No even number found')
+num = int(input('input number : '))
+cnt = 0
 
+# k = 1
+# while k <= num:
+#     if num % k == 0:
+#         cnt += 1
+#     k += 1
+# if cnt == 2:
+#     print(f'{num} is prime number')
+# else:
+#     print(f'{num} is not prime number')
+
+# for k in range(1, num+1):
+#     if num % k == 0:
+#         cnt += 1
+#     k += 1
+# if cnt == 2:
+#     print(f'{num} is prime number')
+# else:
+#     print(f'{num} is not prime number')
+
+# cnt = 0
+# for k in range(2, num):
+#     if num % k:
+#         cnt += 1
+# if cnt:
+#     print(f'{num} is prime number')
+# else:
+#     print(f'{num} is not prime number')
+
+for i in range(2, num):
+    if num % i == 0:
+        print(f'{num} is not prime number')
+        break
+else:
+    print(f'{num} is prime number')

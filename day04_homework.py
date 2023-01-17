e4ea5d47f09a3399805b8abdb33d50489d30f1c3
JiSeong Kim <12180543@inha.edu>
2023-01-17 16:55:46 +0900

    day04_homework2

--- day04_homework.py
+++ day04_homework.py
@@ -1,32 +1,21 @@
-# ch 7
+# ch8
 
-# 7.1 to 7.3
-years_list = [1980, 1981, 1982, 1983, 1984, 1985]
-print(years_list[3])
-print(years_list[-1])
+# 8.1 to 8.5
+e2f = {'dog': 'chien', 'cat': 'chat', 'walrus': 'morse'}
+print(e2f['walrus'])  # 8.2
+f2e = {b: a for (a, b) in e2f.items()}
+print(f2e)  # 8.3
+print(f2e['chien'])  # 8.4
+print(e2f.keys())  # 8.5
 
-# 7.8 to 7.9
-surprise = ['Groucho', 'Chico', 'Harpo']
-print(surprise[-1].lower()[::-1].capitalize())
+# 8.6 to 8.9
+another = {'cats': 'Henri', 'octopi': 'Grumpy', 'emus': 'Lucy'}
+life = {'animals': another, 'plants': {}, 'other': {}}
+print(life)  # 8.6
+print(life.keys())  # 8.7
+print(life['animals'].keys())  # 8.8
+print(life['animals']['cats'])  # 8.9
 
-# 7.10
-even_lst = [i for i in range(10) if i % 2 == 0]
-print(even_lst)
-
-# 7.11
-start1 = ['fee', 'fie', 'foe']
-rhymes = [
-    ('flop', 'get a mop'),
-    ('fope', 'turn the rope'),
-    ('fa', 'get your ma'),
-    ('fudge', 'call the judge'),
-    ('fat', 'pet the cat'),
-    ('fog', 'walk the dog'),
-    ('fun', "say we're done")
-    ]
-start2 = 'Someone better'
-
-for tmp in rhymes:
-    (first, second) = tmp
-    print(*(i.capitalize() + '! ' for i in start1), first.capitalize() + '!', sep='')
-    print(start2, second + '.')
+# 8.10
+squares = {i: i*i for i in range(10)}
+print(squares)

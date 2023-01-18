2d4fe6018900333daeb07ab813457b72d1cf1646
JiSeong Kim <12180543@inha.edu>
2023-01-18 16:13:21 +0900

    day05_homework2

--- day05_homework.py
+++ day05_homework.py
@@ -1,19 +1,10 @@
-# ch 8
+# ch 9
 
-# 8.11
-odd_set = {i for i in range(10) if i % 2 == 1}
-print(odd_set)
-# 8.12
-generator_ = (('Got', i) for i in range(10))
-for i in generator_:
-    print(*i)
-# 8.13
-key_tuple = ('optimist', 'pessimist', 'troll')
-value_tuple = ('The glass is half full', 'The glass is half empty', 'How did you get a glass?')
-new_dict = dict(zip(key_tuple, value_tuple))
-print(new_dict)
-# 8.14
-key_lst = ['Creature of Habit', "crewel Fate"]
-value_lst = ['A num turns into a monster', 'A haunted yarn shop']
-movies = dict(zip(key_lst, value_lst))
-print(movies)
+# 9.1
+def good():
+    return ['Harry', 'Ron', 'Hermione']
+print(good())
+# 9.2
+def get_odds():
+    return [i for i in range(10) if i % 2 ==1]
+print(get_odds()[2])

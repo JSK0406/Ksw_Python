633b1824c9582b0b190a3a77fe615af279df60f2
JiSeong Kim <12180543@inha.edu>
2023-02-03 11:30:21 +0900

    day15_linearlist03

--- day15.py
+++ day15.py
@@ -13,23 +13,29 @@ def insert_data(idx, pokemon):
 
     pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
 
+
+def delete_data(idx):
+    if idx < 0 or idx > len(pokemons):
+        print("데이터를 삭제할 범위를 벗어났습니다.")
+        return
+
+    len_pokemons = len(pokemons)
+    pokemons[idx] = None  # 데이터 삭제
+
+    for i in range(idx + 1, len_pokemons):
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
+
+    del (pokemons[len_pokemons - 1])
+
+
 if __name__ == "__main__":
     insert_data(3, '거북왕')
     print(pokemons)
     insert_data(6, '어니부기')
     print(pokemons)
 
-# pokemons = list()
-#
-# def add_data(pokemon):
-#     pokemons.append(None)
-#     # pokemons[len(pokemons) - 1] = pokemon
-#     pokemons[-1] = pokemon
-#
-# add_data('피카츄')
-# add_data('라이츄')
-# add_data('파이리')
-# add_data('꼬부기')
-# add_data('이상해씨')
-#
-# print(pokemons)
+    delete_data(3)
+    print(pokemons)
+    delete_data(5)
+    print(pokemons)

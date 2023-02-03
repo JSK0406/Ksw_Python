9e9636b4c1b4332535dcc75a3f37c7140ed85021
JiSeong Kim <12180543@inha.edu>
2023-02-03 16:12:16 +0900

    day15_homework

--- day15.py
+++ day15.py
@@ -1,37 +1,22 @@
-class Node():
-    def __init__(self):
-        self.data = None
-        self.link = None
-
-
-def print_nodes(start):
-    current = start
-    if current == None:
-        return
-    print(current.data, end=' ')
-    while current.link != None:
-        current = current.link
-        print(current.data, end=' ')
-    print()
-
-
-lst = []
-head, current, pre = None, None, None
-dataArray = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해씨"]
+def insert_pokemon(name, hp):
+    tmp = [name, hp]
+    i = 0
+    for i in range(len(pokemon_lst)):
+        if pokemon_lst[i][1] < hp:
+            break
+        else:
+            continue
+    else:
+        i += 1
+    pokemon_lst.insert(i, tmp)
 
 if __name__ == "__main__":
-    node = Node()  # 첫 번째 노드
-    node.data = dataArray[0]
-    head = node
-    lst.append(node)
-
-    for data in dataArray[1:]:  # 두 번째 이후 노드
-        pre = node
-        node = Node()
-        node.data = data
-        pre.link = node
-        lst.append(node)
-
-    print_nodes(head)
-    print_nodes(lst[2])
-    print(lst)
+    check = 'A'
+    pokemon_lst = [["파이리", 1000], ["꼬부기", 500], ["이상해씨", 400], ["치코리타", 300], ["이브이", 100]]
+    while check != 'q':
+        name = input("포켓몬 이름 : ")
+        hp = int(input("포켓몬 체력 : "))
+        insert_pokemon(name, hp)
+        print(pokemon_lst)
+        check = input("종료하려면 q / 계속하려면 q 제외 아무키나 : ")
+    print("최종 결과 :", *pokemon_lst)

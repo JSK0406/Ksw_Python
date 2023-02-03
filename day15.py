bd1aa0c7f337829a3ba1d01a5644cc2c72bfa38a
JiSeong Kim <12180543@inha.edu>
2023-02-03 15:03:04 +0900

    day15_linearlist07

--- day15.py
+++ day15.py
@@ -1,52 +1,20 @@
-def print_poly(px):
-    """
-    다항식을 출력하는 함수
-    :param px:
-    :return:
-    """
-    term = len(px) - 1
-    poly_str = "P(x) = "
-
-    for i in range(len(px)):
-        coef = px[i]  # 계수
-
-        if coef == 0:
-            term -= 1
+def insert(name, times):
+    tmp = (name, times)
+    for i in data[::-1]:
+        if times >= i[1]:
             continue
-        if coef > 0 and not i == 0:
-            poly_str += "+"
-        poly_str += f'{coef}x^{term} '
-        term -= 1
+        else:
+            idx = data.index(i) + 1
+            break
+    else:
+        idx = 0
+    data.insert(res, tmp)
 
-    return poly_str
-
-
-def calc_poly(xVal, px):
-    """
-    계산하는 함수
-    :param xVal:
-    :param px:
-    :return:
-    """
-    retValue = 0
-    term = len(px) - 1
-
-    for i in range(len(px)):
-        coef = px[i]  # 계수
-        retValue += coef * xValue ** term
-        term -= 1
-
-    return retValue
-
-px = [7, -4, 0, 5]  # = 7x^3 -4x^2 +0x^1 +5x^0
 
 if __name__ == "__main__":
-    p_str = print_poly(px)
-    print(p_str)
-
-    xValue = int(input("X 값 : "))
-
-    pxValue = calc_poly(xValue, px)
-    print(pxValue)
-
+    data = [('다현', 200), ('정연', 150), ('쯔위', 90), ('사나', 30), ('지효', 15)]
+    insert('미나', 40)
+    print(data)
+    insert('A', 250)
+    print(data)
 

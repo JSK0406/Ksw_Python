d80904c84dde757c100dbcd4ac6b135f45991831
JiSeong Kim <12180543@inha.edu>
2023-02-08 18:05:45 +0900

    day18_homework02

--- day18_homework.py
+++ day18_homework.py
@@ -1,57 +1,89 @@
-# 09-01
+from operator import itemgetter
 
 class Graph:
     def __init__(self, size):
         self.size = size
         self.graph = [[0]*size for _ in range(size)]
 
-store_arr = [['GS25', 30], ['CU', 60], ['Seven11', 10], ['MiniStop', 90], ['Emart24', 40]]
-GS25, CU, Seven11, MiniStop, Emart24 = 0, 1, 2, 3, 4
-g = Graph(len(store_arr))
+city_arr = ['서울', '뉴욕', '런던', '북경', '방콕', '파리']
+서울, 뉴욕, 런던, 북경, 방콕, 파리 = 0, 1, 2, 3, 4, 5
 
-g.graph[GS25][CU] = 1; g.graph[GS25][Seven11] = 1
-g.graph[CU][GS25] = 1; g.graph[CU][Seven11] = 1; g.graph[CU][MiniStop] = 1
-g.graph[Seven11][GS25] = 1; g.graph[Seven11][CU] = 1; g.graph[Seven11][MiniStop] = 1
-g.graph[MiniStop][Seven11] = 1; g.graph[MiniStop][CU] = 1; g.graph[MiniStop][Emart24] = 1
-g.graph[Emart24][MiniStop] = 1
+g = Graph(len(city_arr))
+
+g.graph[서울][뉴욕] = 80; g.graph[서울][북경] = 10
+g.graph[뉴욕][서울] = 80; g.graph[뉴욕][북경] = 40; g.graph[뉴욕][방콕] = 70
+g.graph[런던][방콕] = 30; g.graph[런던][파리] = 60
+g.graph[북경][서울] = 10; g.graph[북경][뉴욕] = 40; g.graph[북경][방콕] = 50
+g.graph[방콕][뉴욕] = 70; g.graph[방콕][북경] = 50; g.graph[방콕][런던] = 30; g.graph[방콕][파리] = 20;
+g.graph[파리][방콕] = 20; g.graph[파리][런던] = 60;
 
 def print_graph(g):
-    print('## 편의점 그래프 ##')
-    print('%9s'%'', end='')
     for i in range(g.size):
-        print("%9s"%store_arr[i][0], end=' ')
+        print("%9s"%city_arr[i], end=' ')
     print('')
     for row in range(g.size):
-        print("%9s"%store_arr[row][0], end=' ')
+        print(city_arr[row], end='')
         for col in range(g.size):
             print("%9d"%g.graph[row][col], end=' ')
         print('')
 
-stack = [0]
-visited_list = []
-current = 0
-max_value = store_arr[0][1]
-
-while stack:
-    next = None
-    for vertex in range(g.size):
-        if g.graph[current][vertex] == 1:
-            if vertex in visited_list:
-                pass
-            else:
-                next = vertex
-                break
-
-    if next != None:
-        current = next
-        stack.append(current)
-        visited_list.append(current)
-    else:
-        current = stack.pop()
-    max_value = store_arr[current][1] if max_value < store_arr[current][1] else max_value
-
+print('## 해저 케이블 연결을 위한 전체 연결도 ##')
 print_graph(g)
 
-for i in store_arr:
-    if i[1] == max_value:
-        print('허니버터칩 최대 보유 편의점(개수) -->', i[0], max_value)
+def find_vertex(g, findvtx):
+    stack = [0]
+    visited_list = [0]
+    current = 0
+
+    while stack:
+        next = None
+        for vertex in range(g.size):
+            if g.graph[current][vertex] != 0:
+                if vertex in visited_list:
+                    pass
+                else:
+                    next = vertex
+                    break
+
+        if next != None:
+            current = next
+            stack.append(current)
+            visited_list.append(current)
+        else:
+            current = stack.pop()
+
+    return True if findvtx in visited_list else False
+
+edge_arr = []
+for start in range(g.size):
+    for end in range(g.size):
+        if g.graph[start][end] != 0:
+            edge_arr.append([g.graph[start][end], start, end])
+
+
+edge_arr = sorted(edge_arr, key=itemgetter(0), reverse=False)
+
+new_arr = []
+for i in range(0, len(edge_arr), 2):
+    new_arr.append(edge_arr[i])
+
+
+idx = 0
+while len(new_arr) > g.size-1:
+    start = new_arr[idx][1]
+    end = new_arr[idx][2]
+    cost = new_arr[idx][0]
+
+    g.graph[start][end] = 0
+    g.graph[end][start] = 0
+
+    if find_vertex(g, start) and find_vertex(g, end):
+        del(new_arr[idx])
+    else:
+        g.graph[start][end] = cost
+        g.graph[end][start] = cost
+        idx += 1
+
+print('')
+print('## 가장 효율적인 해저 케이블 연결도 ##')
+print_graph(g)
